dde6aea055328e7ed5fbf019dea2a204db6a10d1
tsering651 <tseringwang4039@gmail.com>
2022-04-26 13:49:10 +0530

    final and last commit

---
+++ scripts/common.js
@@ -0,0 +1,19 @@
+function showSignInModal() {
+    var modal = document.getElementById("exampleModal2");
+    modal.style.display = "flex";
+  }
+  
+  function hideSignInModal() {
+    var modal = document.getElementById("exampleModal2");
+    modal.style.display = "none";
+  }
+  
+  function showSignUpModal() {
+    var modal = document.getElementById("exampleModal");
+    modal.style.display = "flex";
+  }
+  
+  function hideSignUpModal() {
+    var modal = document.getElementById("exampleModal");
+    modal.style.display = "none";
+  }
